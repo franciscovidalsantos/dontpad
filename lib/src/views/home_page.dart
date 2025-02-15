fb5acbb0b178238ead8f82207509419d3f099efd
francisco <franciscovidalsantos@gmail.com>
2025-02-15 20:42:42 +0000

    adding our home page UI

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -13,21 +13,70 @@ class _MainPageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'dontpad',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            // shadows: [
-            //   Shadow(color: Colors.black, offset: Offset.fromDirection(1, 1.5))
-            // ],
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          // Top section
+          Expanded(
+            child: Container(
+              // color: Colors.blue, // Example color
+            ),
           ),
-        ),
-        backgroundColor: Colors.black,
+
+          // Middle section
+          Expanded(
+            child: Container(
+              // color: Colors.orange,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        "dontpad/",
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Enter your room name...",
+                                border: UnderlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          OutlinedButton(
+                            onPressed: () {
+                              // Navigate to the next page
+                            },
+                            child: Text("Go!"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // Bottom section
+          Expanded(
+            child: Container(
+              // color: Colors.green, // Example color
+            ),
+          ),
+        ],
       ),
-      body: Column(children: []),
     );
   }
 }
